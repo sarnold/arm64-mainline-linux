c90926b0718a39e00c54f2e1b0396e6ed1ac413e
Stephen L Arnold <nerdboy@gentoo.org>
2023-02-20 16:24:34 -0800

    set msm (current) patch version and update base configs
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -41,10 +41,10 @@ toolchain="gcc_12_aarch64"
 
 #Kernel
 KERNEL_REL=6.2
-KERNEL_TAG=${KERNEL_REL}-rc8
+KERNEL_TAG=${KERNEL_REL}
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
