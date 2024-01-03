e2f958f3e7133419658d4171f174f84e994b9989
Stephen L Arnold <nerdboy@gentoo.org>
2024-01-03 14:09:19 -0800

    chg: dev: enable sdhci regulator fix patch for roc-rk3328-cc renegade
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -44,10 +44,10 @@ toolchain="gcc_13_aarch64"
 
 #Kernel
 KERNEL_REL=6.5
-KERNEL_TAG=${KERNEL_REL}.6
+KERNEL_TAG=${KERNEL_REL}.8
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}2
+BUILD=${build_prefix}1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
