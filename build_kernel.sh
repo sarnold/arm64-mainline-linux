c9d3c2a4f8b98be7b65368c78bdd1c6435d3c65f
Stephen L Arnold <sarnold@vctlabs.com>
2026-04-16 21:28:15 -0700

    chg: dev: checkout the workflow and config fragments from v6.9.x
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -41,7 +41,17 @@ copy_defconfig () {
 
 make_menuconfig () {
 	cd "${DIR}/KERNEL" || exit
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" "${config}"
+        if [ -e "$DIR"/patches/fragments ] ; then
+                for file in "${DIR}/patches/fragments"/*.cfg; do
+                        [ -e "$file" ] || continue
+                        fragments+="$file "
+                done
+                scripts/kconfig/merge_config.sh -m "arch/${KERNEL_ARCH}/configs/${config}" "$fragments"
+                make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" olddefconfig
+        else
+                make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" "${config}"
+        fi
+
 	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" menuconfig
 	./scripts/config --disable CONFIG_LOCALVERSION_AUTO
 	./scripts/config --disable CONFIG_DEBUG_INFO
