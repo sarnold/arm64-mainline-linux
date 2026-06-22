1d28f6668e9f894feff2335ce9d4b0c30c95b635
Stephen L Arnold <sarnold@vctlabs.com>
2026-06-21 20:00:23 -0700

    fix: dev: restore missing mvebu USB driver and bump build rev again
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}5
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
